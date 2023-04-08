67077a5ba2487afd68055f7e2e5110ca63c5f8dd
dev-naf <ahnaf.fattah1@gmail.com>
2023-04-08 11:28:02 +0700

    Menampilkan data

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const TodoItem = (props) => {
+const TodoItem = (todo) => {
     return (
         <div className='border border- border-info rounded mb-3 p-3 d-flex justify-content-between'>
             <div>
-                <h5 className='fs-5 mt-1'>{props.tugas}</h5>
-                <p className='fs-6 lead'>{props.tanggal}</p>
+                <h5 className='fs-5 mt-1'>{todo.tugas}</h5>
+                <p className='fs-6 lead'>{todo.tanggal}</p>
             </div>
             <div>
                 <button className='btn btn-danger mt-3'><i class="bi bi-trash3-fill"></i></button>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import TodoItem from './TodoItem'
 
 const TodoList = () => {
+  // State data to-do
+  const [todo,setTodo] = useState([])
+  
+  useEffect(()=>{
+    // ambil data todo
+    fetch('http://localhost:8080/todo')
+    .then((response) => response.json())
+    .then((data) => setTodo(data))
+    .catch(error => console.log(error))
+  })
+
   return (
     <div id="todo-list" className='mt-4'>
-        <TodoItem tugas="mencuci pakaian" tanggal="12 Januari 2024"/>
-        <TodoItem tugas="mencuci piring" tanggal="13 Januari 2024"/>
-        <TodoItem tugas="mencuci Handuk" tanggal="14 Januari 2024"/>
+        {todo.map(
+          (Todo) =>(
+            <TodoItem id={Todo.key} tugas={Todo.tugas} tanggal={Todo.tanggal}/>
+          )
+        )}
     </div>
   )
 }
